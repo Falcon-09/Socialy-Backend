d87202e9d0ff5f52ab833cd30fa08754b2f04a3d
Falcon-09 <nipunairr1@gmail.com>
2022-10-18 14:04:50 +0530

    cors_fix

--- index.js
+++ index.js
@@ -20,13 +20,9 @@ const app = express();
 // middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 const corsOptions ={
-  origin:'*', 
+  origin:'https://1-socialy-1.netlify.app', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200,
   allowedHeaders: [
