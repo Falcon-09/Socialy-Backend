e3136292b57faaa7dce74e701d3856a2d155a3e9
Nipun_Mitra <57940556+Falcon-09@users.noreply.github.com>
2022-12-02 16:21:48 +0530

    deleteChat

--- routes/ChatRoute.js
+++ routes/ChatRoute.js
@@ -5,5 +5,6 @@ const router = express.Router()
 router.post('/', createChat);
 router.get('/:userId', userChats);
 router.get('/find/:firstId/:secondId', findChat);
+router.delete('/:firstId/:secondId', deleteChat);
 
 export default router
